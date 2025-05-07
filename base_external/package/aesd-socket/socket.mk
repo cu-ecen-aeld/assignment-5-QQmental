76e25102d5c76d2f643b558bb0eb02c2f83db662
QQmental <kukumantou1288@gmail.com>
2025-05-07 21:46:23 +0800

    ...

--- base_external/package/aesd-socket/socket.mk
+++ base_external/package/aesd-socket/socket.mk
@@ -1,31 +1,32 @@
 
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# AESD-SOCKET
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'a6130dcfc8113d3c59d2fda513f7959f2b9143e6'
+AESD_SOCKET_VERSION = 2980e978b1ca0e0535d18acc10fd423acbc2099a
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-QQmental.git'
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESD_SOCKET_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-QQmental.git
+AESD_SOCKET_SITE_METHOD = git
+AESD_SOCKET_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+define AESD_SOCKET_BUILD_CMDS
+	$(MAKE) -C $(@D)/server
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+define AESD_SOCKET_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment5/* $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/etc/finder-app
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/etc/finder-app
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/etc/finder-app	
+	$(INSTALL) -D -m 0755 $(@D)/full-test.sh $(TARGET_DIR)/usr/bin/full-test.sh
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S88aesdsocket
 endef
 
 $(eval $(generic-package))
